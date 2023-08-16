76f0d2bf129d8e1a414be54fc63e1785152b8813
PedroBitencourt <pedro.bitencourt42@gmail.com>
2023-08-15 21:22:34 -0300

    Add basic unit test

---
+++ src/test/java/com/example/swplanetapi/utils/PlanetUtils.java
@@ -0,0 +1,8 @@
+package com.example.swplanetapi.utils;
+
+import com.example.swplanetapi.model.Planet;
+
+public class PlanetUtils {
+
+    public static final Planet PLANET = Planet.builder().name("Name").climate("climate").terrain("terrain").build();
+}
